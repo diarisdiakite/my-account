d25a84b1b927583eb499cb2cf3839582c88d78dc
diarisdiakite <diarisdiakite@gmail.com>
2023-11-22 10:08:28 +0100

    Generate Category MVC scaffold.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ﻿Rails.application.routes.draw do
+  resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   devise_for :users
---
+++ test/factories/categories.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :category do
+    user { nil }
+    icon { "MyString" }
+  end
+end
