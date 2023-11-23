3bb3728a1318aaa022f1c311bd0497273f504f2b
diarisdiakite <diarisdiakite@gmail.com>
2023-11-23 12:04:02 +0100

    Update migrations files.

--- db/migrate/20231024013212_add_indexes_to_recipes_and_likes.rb
+++
@@ -1,7 +0,0 @@
-﻿class AddIndexesToRecipesAndLikes < ActiveRecord::Migration[7.0]
-  def change
-    unless index_exists?(:recipes, :user_id)
-      add_index :recipes, :user_id
-    end
-  end
-end
--- db/migrate/20231108034127_add_devise_to_users.rb
+++ db/migrate/20231108034127_add_devise_to_users.rb
@@ -31,16 +31,12 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
       # t.datetime :locked_at
 
 
-      t.timestamps null: false
+      # t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
-    end
-
-    # add_index :users, :email,                unique: true
-    # add_index :users, :reset_password_token, unique: true
   end
 end
