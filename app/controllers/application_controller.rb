6181b64400879f4d37ed49b17e3ad901a5184614
diarisdiakite <diarisdiakite@gmail.com>
2023-12-02 17:39:42 +0100

    Add the splash controller. Define the root at the splash index.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: -> { request.controller_class == SplashController }
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -13,5 +13,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) do |user_params|
       user_params.permit(:name, :bio, :email, :password, :current_password)
     end
+  end
+
+  def after_sign_in_path_for(resource)
+    user_path(resource) # adjust this line based on your routes
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   end
     
   # Defines the root path route ("/")
-  root "categories#index"
+  root "splash#index"
 end
